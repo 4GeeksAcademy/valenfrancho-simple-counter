ca89d4562cafb50a258e6115feb89d636ea18d38
Valentina Franciosi <126563681+valenfrancho@users.noreply.github.com>
2023-07-10 21:31:56 +0200

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import SimpleCounter from "./simple-counter"
-import Timer from "./timer"
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
 			<SimpleCounter/>
-			<Timer/>
 		</div>
 	);
 };
